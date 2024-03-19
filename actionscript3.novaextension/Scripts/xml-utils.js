ac9e37b3769ca18c090e8338999fd511b940829b
Christopher Pollati <abattoir@optonline.net>
2024-03-19 08:32:44 -0700

    Prevents errors on import FB prjoect. Still not best solution.

--- actionscript3.novaextension/Scripts/xml-utils.js
+++ actionscript3.novaextension/Scripts/xml-utils.js
@@ -1,5 +1,23 @@
-exports.getTag = function(xml, tag) { return String(xml.select("//"+tag)).trim(); }
+exports.getTag = function(xml, tag) {
+	try {
+		return String(xml.select("//"+tag)).trim();
+	} catch(e) {
+		return "";
+	}
+}
 
-exports.getTagAttribute = function(xml, tag, attr) { return String(xml.select("//"+tag)["attributes"][attr]).trim(); }
+exports.getTagAttribute = function(xml, tag, attr) {
+	try {
+		return String(xml.select("//"+tag)["attributes"][attr]).trim();
+	} catch(e) {
+		return "";
+	}
+}
 
-exports.getAttribute = function(xml, attr) { return String(xml["attributes"][attr]).trim(); }
+exports.getAttribute = function(xml, attr) {
+	try {
+		return String(xml["attributes"][attr]).trim();
+	} catch(e) {
+		return "";
+	}
+}
